1b8824a01d0fa85d2b1663399167eba781b84791
SupakVetanat <ve.supak_st@tni.ac.th>
2022-11-19 12:06:44 +0700

    index2

---
+++ index2.js
@@ -0,0 +1,14 @@
+const drinks =['Greentea','Coffee','Coke']
+// for (const d of drinks){
+//     console.log(d)
+// }
+
+const users = [
+    {name:'A',age:16},
+    {name:'B',age:18},
+    {name:'C',age:20},
+]
+
+for(const user of users){
+    console.log(`Username ${user.name}`)
+}
